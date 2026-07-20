8c4520d2434c22cdbadad0924e29a2f6eab4cd9c
Anderson Silva <andersongustavomelo@gmail.com>
2026-07-20 11:03:23 -0400

    Parte1: Understanding and Creating Objects

---
+++ script.js
@@ -0,0 +1,18 @@
+// Part 1: Understanding and Creating Objects
+let student = {
+    name: "Anderson",
+    age: 31,
+    enrolled: true,
+    courses: ["Web programming and Design", "Fundamentals of Java Script", "Algorithms and Problem Solving", "Operating System", "Technical Wrt", "Database"],
+    info: {
+        country: "Canada",
+        city: "Toronto",
+        street: "Maple Leaf Street",
+        college: "Humber College",
+        program: "Computer Programming"
+    }
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.info);
