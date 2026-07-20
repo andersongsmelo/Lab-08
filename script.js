afdb5ed108a6a581320a3f1e4c024087ed19bc83
Anderson Silva <andersongustavomelo@gmail.com>
2026-07-20 11:27:08 -0400

    Part 2: Working with JSON

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ let student = {
 console.log(student.name);
 console.log(student.age);
 console.log(student.info);
+
+// Part 2: Working with JSON
+// Convert the student object into a JSON string
+let jsonString = JSON.stringify(student);
+console.log(jsonString);
+// Convert the JSON string back into a JavaScript object.
+let jsonObj = JSON.parse(jsonString);
+console.log(jsonObj);
+//Compare
+console.log(student === jsonObj);
